0222353b7fa8e51ba55a20dad4f9a0e3077bac66
Pedro Terra <pedrooterra@outlook.com>
2024-06-18 08:45:41 -0300

    feat: correção de uma falha

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let numeros = [3, 1, 3, 2, 4, 4, 5, 2, 1, 6];
+let numeros = [3, 1, 3, 2, 4, 4,4, 4, 5, 2, 1, 6];
 let numerosO = [];
 
 for (let i = 0; i < numeros.length; i++) {
@@ -15,6 +15,7 @@ for (let i = 0; i < numeros.length; i++) {
   for (let j = i + 1; j < numeros.length; j++)
     if (numeros[i] == numeros[j]) {
       numeros.splice(i, 1);
+      j--
     }
 }
 
